0e7ed944333df5f2372b74e93e499ba92f797c89
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2024-05-27 12:26:36 +0100

    :sparkles: Implement Redux & Snackbar (#6)
    * Add redux dependencies and create test page with incrementing counter
    
    * Get toast stacking working
    
    * Get toast stacking working with Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { ThemeProvider } from '@mui/material';
-import { theme } from './theme';
-import App from './app';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./theme";
+import App from "./app";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { CssVarsProvider } from '@mui/material-next';
-
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { CssVarsProvider } from "@mui/material-next";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 
 root.render(
   <React.StrictMode>
-    {/* <CssVarsProvider theme={...}> */}
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-    {/* </CssVarsProvider> */}
+    <Provider store={store}>
+      {/* <CssVarsProvider theme={...}> */}
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+      {/* </CssVarsProvider> */}
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/state/root-reducer.ts
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import toast from "./toast";
+
+export default combineReducers({
+  toast,
+});
